d6aa2deb386eeac1aa5b1aaf1bdf9786bc8c3468
BangDDong <gusehdqkd@naver.com>
2022-04-20 22:12:01 +0900

    Add - Validation
    * spring validation 추가

--- tracker-core/src/main/java/com/bd/tracker/core/dto/BatchInfoRequest.java
+++ tracker-core/src/main/java/com/bd/tracker/core/dto/BatchInfoRequest.java
@@ -4,13 +4,19 @@ import com.bd.tracker.core.entity.BatchInfo;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @RequiredArgsConstructor
 public class BatchInfoRequest {
 
+    @NotBlank
     private final String url;
+    @NotBlank
     private final String cssQuery;
+    @NotBlank
     private final String category;
+    @NotBlank
     private final String productNm;
 
     public BatchInfo toEntity() {
